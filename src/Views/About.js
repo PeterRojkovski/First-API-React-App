b00b489a127d207c4777aec8d384cb7fbdc75e17
Peter Rojkovski <84549374+PeterRojkovski@users.noreply.github.com>
2022-04-26 02:08:28 -0600

    Update About.js

--- src/Views/About.js
+++ src/Views/About.js
@@ -5,7 +5,7 @@ function About(){
         <div className="relative">
             <h1 className="font-bold text-2xl mb-3">About</h1>
             <p>
-                This is the page all about ME. JUST ME.
+                About me page!!
             </p>
         </div>
     )
