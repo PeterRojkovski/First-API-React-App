aecb78e52012d812d701b38161cb31722fa9d9c3
Peter Rojkovski <84549374+PeterRojkovski@users.noreply.github.com>
2022-04-26 02:13:52 -0600

    Update Home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -7,6 +7,9 @@ function Home(){
             <div className="relative">
             <h1 className="font-bold text-2xl mb-3">Home</h1>
             <HelloWorld name="Peter"/>
+            <p>
+                To use the product feature of the website, add '/products/{product number}' to the url, where {product number} is any integer from 1 to 39!
+            </p>
         </div>
         
     )
