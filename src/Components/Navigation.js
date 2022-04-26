30b1f9b33bea9c4565c9debbf0d5180647285030
Peter Rojkovski <84549374+PeterRojkovski@users.noreply.github.com>
2022-04-26 03:06:03 -0600

    Update Navigation.js

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faBars} from '@fortawesome/free-solid-svg-icons'
-import {useTransition, animated} from 'react-spring'
 import {
     Link
   } from "react-router-dom"
